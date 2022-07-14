f4fc9532c0210caa733cbe69f79dc0ef23672222
merthanmerter <merthanm@gmail.com>
2022-07-14 08:20:30 +0300

    main

--- pages/api/projects/[id].ts
+++ pages/api/projects/[id].ts
@@ -5,7 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method, query } = req
 
-  const id = query.id || ''
+  const id: any = query.id || ''
 
   let response: any
 
